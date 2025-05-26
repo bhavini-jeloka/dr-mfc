bc4562c70c4efd90b3b773bcf21363e577740ff1
bhavini-jeloka <jelokab@gmail.com>
2025-05-26 12:48:10 -0400

    add new file for seeded performance

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run the dynamics_toy_example module with Python -m
-python3 -m mf_estimation.grid_nav_example.grid_navigation_example
+python3 -m mf_estimation.grid_nav_example.grid_navigation_example_multi_seeds
