fix l1 formula

mf_estimation/larger_grid_example/large_grid_example.py
@@ -68,7 +68,7 @@ for idx, num_comm_rounds in enumerate(comm_rounds_list):
         for t in range(num_timesteps):
             print("Seed:", seed, "Timestep:", t)
             
-            l1_errors_all_seeds[seed, t] = np.sum(np.abs(mean_field - des_mean_field))
+            l1_errors_all_seeds[seed, t] = 0.5*np.sum(np.abs(mean_field - des_mean_field))
             
             # Rewards
             rewards_actual_all_seeds[seed, t] = dynamics.compute_reward()