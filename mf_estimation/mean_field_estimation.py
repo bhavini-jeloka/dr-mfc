5cfa860dc1c04a6c0545dd179477dbb0a90f8d0e
bhavini-jeloka <bhavini.jeloka@gmail.com>
2025-06-06 11:35:57 -0400

    no noise

--- mf_estimation/mean_field_estimation.py
+++ mf_estimation/mean_field_estimation.py
@@ -21,7 +21,7 @@ class MeanFieldEstimator():
         self.state_info = {i: [] for i in range(self.num_states)}
         self.estimate_history = {state: [] for state in range(self.num_states)}
 
-        self.noise_std = 0.01
+        self.noise_std = 0.0
 
     def sample_particles(self, fixed_indices, fixed_values):
         self.weights =  {i: np.ones(self.num_particles)/self.num_particles for i in range(self.num_states)}
