6a71656f9f0a7a725ca78300824278f4d5f38ca8
bhavini-jeloka <bhavini.jeloka@gmail.com>
2025-06-11 14:43:19 -0400

    filter files

--- mf_estimation/finite_population_grid_nav/test_grid_nav.py
+++ mf_estimation/finite_population_grid_nav/test_grid_nav.py
@@ -59,10 +59,14 @@ if __name__ == "__main__":
         match = re.search(r"config_(\d+)\.json", path)
         return int(match.group(1)) if match else -1
 
-    config_files = sorted(glob.glob("configs/config_*.json"), key=numeric_key)
+    # All matching files
+    all_files = glob.glob("configs/config_*.json")
 
-    for config_path in config_files:
-        print(f"\nLoading config: {config_path}")
-        with open(config_path, "r") as jsonfile:
-            config = json.load(jsonfile)
-        main(config)
+    # Filter files in range 17 to 24
+    filtered_files = [
+        f for f in all_files
+        if (match := re.search(r"config_(\d+)\.json", f)) and 17 <= int(match.group(1)) <= 24
+    ]
+
+    # Sort them numerically
+    config_files = sorted(filtered_files, key=numeric_key)
--- mf_estimation/post_processing/plotting_finite_population.py
+++ mf_estimation/post_processing/plotting_finite_population.py
@@ -15,27 +15,37 @@ for idx, num_comm_rounds in enumerate(comm_rounds_list):
 
 
     save_dir_est = f"mean_field_trajectory/grid_{grid[0]}x{grid[1]}_partial_obs_True_comm_{num_comm_rounds}"
-    save_dir_des = f"mean_field_trajectory/grid_{grid[0]}x{grid[1]}_partial_obs_False_comm_{num_comm_rounds}"
+    save_dir_ben = f"mean_field_trajectory/grid_{grid[0]}x{grid[1]}_partial_obs_True_comm_{num_comm_rounds}_benchmark"
+    save_dir_des = f"mean_field_trajectory/grid_{grid[0]}x{grid[1]}_partial_obs_False"
 
-    l1_dpc = np.zeros((num_timesteps, num_episodes))
-
+    l1_dpc = np.zeros((num_episodes, num_timesteps))
+    l1_benchmark = np.zeros((num_episodes, num_timesteps))
 
     for ep in range(num_episodes):
 
         output_est = os.path.join(save_dir_est, f"ep_{ep}.npy")
+        output_ben = os.path.join(save_dir_ben, f"ep_{ep}.npy")
         output_des = os.path.join(save_dir_des, f"ep_{ep}.npy")
 
         estimated_mf = np.load(output_est)
+        benchmark_mf = np.load(output_ben)
         des_mf = np.load(output_des)
 
         print(estimated_mf.shape)
+        print(output_ben.shape)
         print(des_mf.shape)
+
+        # fill missing entries with last l1 value and make a marking
+
+        # compute l1_error and store for each time step
         assert(1==0)
 
     avg_l1_dpc = l1_dpc.mean(axis=0)
+    avg_l1_benchmark = l1_benchmark.mean(axis=0)
 
     # Plot
     axs[idx].plot(avg_l1_dpc, label='D-PC (Ours)')
+    axs[idx].plot(avg_l1_benchmark, label='Benchmark')
     axs[idx].set_title(f'Comm Rounds: {num_comm_rounds}')
     axs[idx].set_xlabel('Time')
     axs[idx].set_ylabel('Total Variation Error')
