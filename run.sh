dcb7a3e783d87bd4ac7f07c3c8ac7ccb71825add
bhavini-jeloka <bhavini.jeloka@gmail.com>
2025-06-15 10:31:04 -0400

    test 1000 finite pop

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # Run the dynamics_toy_example module with Python -m
-python3 -m mf_estimation.larger_grid_example.large_grid_example
-python3 -m mf_estimation.larger_grid_example.large_grid_example_benchmark
+python3 -m mf_estimation.finite_population_grid_nav.test_grid_nav
