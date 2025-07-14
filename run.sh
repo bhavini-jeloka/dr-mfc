a3161d846fc3547525e0119495e43a03b63d4964
bhavini-jeloka <bhavini.jeloka@gmail.com>
2025-07-14 11:09:49 +0530

    change shell file

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run the dynamics_toy_example module with Python -m
-python3 -m mf_estimation.post_processing.plotting_finite_lcp_relation
+python3 -m mf_estimation.finite_population_grid_nav.test_grid_nav
